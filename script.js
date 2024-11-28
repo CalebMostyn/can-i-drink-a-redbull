e8cdd412095b363569b8ebcf8b5a7f03a2b85a79
CalebMostyn <mostyncr@miamioh.edu>
2024-11-28 00:47:29 -0500

    Fixing JS Issues

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const DB_GOOGLE_SHEET_ID = "1q96-1BnIxsPl5oeTGrgv8OUeiH25wxU5qbmSHyo9QQU";
 const main_text = document.getElementById('text-div');
 
 function printGoogleSheets() {
-    
-
     let cells = "";
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${DB_GOOGLE_SHEET_ID}/values/Sheet1${cells}?key=${MY_API_KEY}`)
         .then(response => response.json())
