4b5b6c2e423ab41b13ac13dbe42ecd8797258a5f
CalebMostyn <mostyncr@miamioh.edu>
2024-11-28 00:38:13 -0500

    Update Text Via JS

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const MY_API_KEY = "AIzaSyDnsbjtNe0DWpC0TVPtwV4uskN_yZCnPw4"; // please nobody steal this
 const DB_GOOGLE_SHEET_ID = "1q96-1BnIxsPl5oeTGrgv8OUeiH25wxU5qbmSHyo9QQU";
+const main_text = document.getElementById('text-div');
 
 function printGoogleSheets() {
+    
+
     let cells = "";
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${DB_GOOGLE_SHEET_ID}/values/Sheet1${cells}?key=${MY_API_KEY}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => main_text.innerText = data);
 }
