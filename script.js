811c0f5a74461b4f6306d4caa4c87b09ba4df5c2
CalebMostyn <mostyncr@miamioh.edu>
2024-11-28 00:10:03 -0500

    Testing Google Sheets API

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const MY_API_KEY = "AIzaSyDnsbjtNe0DWpC0TVPtwV4uskN_yZCnPw4";
+const MY_API_KEY = "AIzaSyDnsbjtNe0DWpC0TVPtwV4uskN_yZCnPw4"; // please nobody steal this
+const DB_GOOGLE_SHEET_ID = "1q96-1BnIxsPl5oeTGrgv8OUeiH25wxU5qbmSHyo9QQU";
 
 function printGoogleSheets() {
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/1q96-1BnIxsPl5oeTGrgv8OUeiH25wxU5qbmSHyo9QQU/values/Sheet1?key=${MY_API_KEY}`)
+    let cells = "";
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${DB_GOOGLE_SHEET_ID}/values/Sheet1!${cells}?key=${MY_API_KEY}`)
         .then(response => response.json())
         .then(data => console.log(data));
 }
